150fa7bb07a0ea1f72ea5959b94af1cc8f06462e
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-01-31 14:04:19 +0400

    API functions

---
+++ Test/script.js
@@ -0,0 +1,9 @@
+function english() {
+    document.getElementById('headline').innerHTML = 'Hello';
+    document.getElementById('text1').innerHTML = 'Welcome on my awesome page!';
+}
+
+function deutsch() {
+    document.getElementById('headline').innerHTML = 'Hallo';
+    document.getElementById('text1').innerHTML = 'Hallo auf dieser tollen Seite!';
+}
